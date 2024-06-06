ca560cd376b44ea5f36c4f4556e8b4847262e276
pooriaparhizkar <pooriaparhizkar@yahoo.com>
2024-06-06 16:28:53 +0330

    Add: styles and icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import "./App.css";
 import wordsData from "./words_output.json";
 import { useEffect, useState } from "react";
+import { Button, DatePicker, Collapse } from "antd";
+import dayjs from "dayjs";
+import { LeftOutlined, RightOutlined, SoundOutlined } from "@ant-design/icons";
+import "./App.scss";
 
 function App() {
   const PERIOD = 14;
@@ -11,26 +14,71 @@ function App() {
     result.setDate(result.getDate() + days);
     return result;
   }
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
+
+  const panelStyle = {
+    marginBottom: 16,
+    border: "none",
+    boxShadow: "box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;",
+    background: "rgba(255, 255, 255, 0.01)",
+    borderRadius: 8,
+    overflow: "hidden",
+  };
+
+  function childrenCreator(item) {
+    return (
+      <div>
+        <span>{item.role}</span>
+        <h5>{item.description}</h5>
+        <h6>{item.translate}</h6>
+        {item.example && item.example.length !== 0 && (
+          <>
+            <span>Examples:</span>
+            {item.example.map(
+              (item, index) =>
+                index < 3 && (
+                  <div key={index} className="exampleContainer">
+                    <p>
+                      {index + 1} - {item}
+                    </p>
+                  </div>
+                )
+            )}
+          </>
+        )}
+        <div className="soundContainer">
+          {item.pronounce_audio_us_link !== "Audio not found" && (
+            <div
+              onClick={() => playAudio(item.pronounce_audio_us_link)}
+              className="sound"
+            >
+              US
+              <SoundOutlined />
+            </div>
+          )}
+          {item.pronounce_audio_uk_link !== "Audio not found" && (
+            <div
+              onClick={() => playAudio(item.pronounce_audio_uk_link)}
+              className="sound"
+            >
+              UK
+              <SoundOutlined />
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 
   useEffect(() => {
     const wordsList = [];
     let temp = [];
     wordsData.map((item, index) => {
-      temp.push(item);
+      temp.push({
+        key: index,
+        label: item.title,
+        children: childrenCreator(item),
+        style: panelStyle,
+      });
       if ((index + 1) % (wordsData.length / PERIOD) === 0) {
         wordsList.push(temp);
         temp = [];
@@ -46,25 +94,29 @@ function App() {
     new Audio(link).play();
   }
   return (
-    <div className="App">
+    <div className="dictionary">
       <h1>Pooria's Dictionary</h1>
-      <div style={{ display: "flex" }}>
-        <button onClick={() => changeDay(-1)}>Previous</button>
-        <h4>
-          {months[currentDate.getMonth()]} {currentDate.getDate()}
-        </h4>
-        <button onClick={() => changeDay(+1)}>Next</button>
+      <div className="controller">
+        <Button type={"primary"} onClick={() => changeDay(-1)}>
+          <LeftOutlined />
+        </Button>
+        <DatePicker
+          allowClear={false}
+          value={dayjs(currentDate)}
+          onChange={(e) => setCurrentDate(new Date(e))}
+        />
+        <Button type={"primary"} onClick={() => changeDay(+1)}>
+          <RightOutlined />
+        </Button>
       </div>
-
+      <Collapse
+        className="words"
+        size="large"
+        items={words[currentDate.getDate() % PERIOD]}
+        defaultActiveKey={["1"]}
+      />
       {words[currentDate.getDate() % PERIOD]?.map((item, index) => (
-        <div
-          style={{
-            borderTop: "solid 1px gray",
-            paddingTop: "16px",
-            marginTop: "16px",
-          }}
-          key={index}
-        >
+        <div key={index}>
           <span>{index + 1}</span>
           <h4>{item.title}</h4>
           <h6>{item.description}</h6>
